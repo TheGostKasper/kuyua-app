6d838f57c614c742c25c03d65e2b4dfa1bfb0ca9
TheGostKasper <thegostkasper@gmail.com>
2024-08-03 16:08:28 +0300

    Refactor LocationList and MapContainer components to handle Error and loading status

--- client/src/components/LocationList.tsx
+++ client/src/components/LocationList.tsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { useLazyTable } from "../hooks/useLazyTable";
-import { useFetchLocations } from "../hooks/useFetchLocation";
+import { DataTable } from "primereact/datatable";
+import React from "react";
 import { useUserLocation } from "../contexts/UserLocationContext";
+import { useFetchLocations } from "../hooks/useFetchLocation";
+import { useLazyTable } from "../hooks/useLazyTable";
+import { Error } from "../shared/loadingError";
 
 export const LocationList: React.FC = () => {
   const { lazyState, onPage, onSort, onFilter } = useLazyTable();
   const { setLocation } = useUserLocation();
 
-  const { data, isLoading } = useFetchLocations({
+  const { data, isLoading, isError } = useFetchLocations({
     search: lazyState.filters.name?.value,
     sortBy: lazyState.sortField,
     sortDirection: lazyState.sortOrder === 1 ? "asc" : "desc",
@@ -25,36 +26,39 @@ export const LocationList: React.FC = () => {
   return (
     <div className="card">
       <h2>Locations </h2>
-      <DataTable
-        value={data?.locations || []}
-        lazy
-        selectionMode="single"
-        filterDisplay="row"
-        dataKey="id"
-        paginator
-        first={lazyState.first}
-        rows={lazyState.rows}
-        totalRecords={data?.total || 0}
-        onPage={onPage}
-        onSort={onSort}
-        sortField={lazyState.sortField}
-        sortOrder={lazyState.sortOrder === 1 ? 1 : -1}
-        onFilter={onFilter}
-        filters={lazyState.filters}
-        loading={isLoading}
-        tableStyle={{ minWidth: "75rem" }}
-        onRowClick={handleRowClick}
-      >
-        <Column
-          field="name"
-          header="Name"
-          sortable
-          filter
-          filterPlaceholder="Search by name"
-        />
-        <Column field="score" header="Score" sortable />
-        <Column field="address" header="Address" />
-      </DataTable>
+      {isError && <Error />}
+      {!isError && (
+        <DataTable
+          value={data?.locations || []}
+          lazy
+          selectionMode="single"
+          filterDisplay="row"
+          dataKey="id"
+          paginator
+          first={lazyState.first}
+          rows={lazyState.rows}
+          totalRecords={data?.total || 0}
+          onPage={onPage}
+          onSort={onSort}
+          sortField={lazyState.sortField}
+          sortOrder={lazyState.sortOrder === 1 ? 1 : -1}
+          onFilter={onFilter}
+          filters={lazyState.filters}
+          loading={isLoading}
+          tableStyle={{ minWidth: "75rem" }}
+          onRowClick={handleRowClick}
+        >
+          <Column
+            field="name"
+            header="Name"
+            sortable
+            filter
+            filterPlaceholder="Search by name"
+          />
+          <Column field="score" header="Score" sortable />
+          <Column field="address" header="Address" />
+        </DataTable>
+      )}
     </div>
   );
 };
